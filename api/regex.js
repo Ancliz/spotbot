f7ebd9a104c0f2a5438158d58dbe4a860eab1167
Ancliz <ancliz@ancliz.me>
2025-02-11 20:44:19 +1100

    feat: added getting track by uri

---
+++ api/regex.js
@@ -0,0 +1,3 @@
+export const TRACK_ID = /(?<=track[:\/])[a-zA-Z0-9]+(?=\?|[a-zA-Z0-9]*)/;
+export const TRACK_URI = /^spotify:track:[a-zA-z0-9]+$/;
+export const TRACK_URL = /https:\/\/open\.spotify\.com\/track\//;
--- api/search.js
+++ api/search.js
@@ -1,5 +1,5 @@
 import RequestBuilder, { BadRequestException, httpException, HttpStatus, UnauthorizedException } from "../global/requests-util.js";
-
+import { TRACK_ID } from "./regex.js";
 
 export async function search(token, args) {
     let url = `https://api.spotify.com/v1/search?type=${args[0]}&q=${args[1]}&limit=${args[2]}`;
@@ -22,4 +22,20 @@ export async function search(token, args) {
         throw httpException(response.status, result.error.message);
     }
 
+}
+
+
+export async function getTrack(token, uri) {
+    const id = uri.match(TRACK_ID);
+    const request = new RequestBuilder()
+        .url(`https://api.spotify.com/v1/tracks/${id}`)
+        .headers({"Authorization": `Bearer ${token}`})
+        .build();
+    const response = await request();
+    const data = await response.json();
+    if(response.status === HttpStatus.OK) {
+        return data;
+    } else {
+        throw httpException(response.status, data.error.message);
+    }
 }
